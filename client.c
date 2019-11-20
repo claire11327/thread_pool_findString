753e77381bad6bdb3eff2745b377878901555555
claire <linda21329@gmail.com>
2019-11-20 15:41:56 +0800

    input formulate: Query "string"

--- client.c
+++ client.c
@@ -21,93 +21,113 @@ void client_thread(CTask* ctask);
 
 int main(int argc, char *argv[]){
 
-     if (argc < 5) {
-         printf("ERROR,please enter [./client -h host -p port]\n");
-         exit(1);
-     }
+	if (argc < 5) {
+		printf("ERROR,please enter [./client -h host -p port]\n");
+		exit(1);
+	}
 
 	/* value var */ 
-     char *ip = argv[2];
-     int port = atoi(argv[4]);
+	char *ip = argv[2];
+	int port = atoi(argv[4]);
 
-     char buffer[MAXLen]; 
-     char msg[MAXLen]; 
-     int sockfd = 0;
-     int numbytes = 0;
+	char buffer[MAXLen]; 
+	char msg[MAXLen]; 
+	int sockfd = 0;
+	int numbytes = 0;
 
 
+	const char * const delim = "\"";
 
-     while(1){
-	     if(fgets(msg, sizeof(msg), stdin) != NULL){
-		     if(strlen(msg) >= 256){
-			     printf("you can only search string under size 255\n");
-				     memset(msg, '\0', strlen(msg)+1);
+	while(1){
+		if(fgets(msg, sizeof(msg), stdin) != NULL){
+			if(strlen(msg) >= 256){
+				printf("you can only search string under size 255\n");
+				memset(msg, '\0', strlen(msg)+1);
 			}
-			     else{
-				     printf("input msg : %s\n",msg);
+			else{
+				if(msg[strlen(msg)-1] == '\n')
+					msg[strlen(msg)-1] = '\0';
+				printf("input msg : [%s]\n",msg);
 
-				     //cut str
+				//cut str
 
-				     if(msg[strlen(msg)-1] == '\n')
-					     msg[strlen(msg)-1] == '\0';
+				//char buf[30] = "Query \"test\" \"te\" \"e\" \"w\" ";
+				char *substr = NULL;
+				char *saveptr = NULL;
+				int count = 0;
+				substr = strtok_r(msg, delim, &saveptr);
+				if(strcmp(substr,"Query ") == 0){
+					substr = strtok_r(NULL, delim, &saveptr);
+					do {
+						printf("sub string : [%s]\n",substr);
+						CTask* ctask = malloc(sizeof(struct CTask));
+						ctask->str = malloc(strlen(substr)+1);
 
+						strcpy(ctask->str,substr); 
+						ctask->ip = ip;
+						ctask->port = port;
 
-				     CTask* ctask = malloc(sizeof(struct CTask));
-				     ctask->str = malloc(strlen(msg));
-				     strcpy(ctask->str,msg); 
-				     ctask->ip = ip;
-				     ctask->port = port;
+						/* create a thread to handle query (write to server) */	
+						pthread_t t;
+						pthread_create(&t, NULL, (void*)client_thread, ctask);
+						memset(msg, '\0', strlen(msg)+1);
 
-				     /* create a thread to handle query (write to server) */	
-				     pthread_t t;
-				     pthread_create(&t, NULL, (void*)client_thread, ctask);
-
-				     memset(msg, '\0', strlen(msg)+1);
-			     }
-		     }
-
-	     }
-
-
-     }
-     void client_thread(CTask* ctask){
-
-	     char* server_ip  = malloc(strlen(ctask->ip));
-	     strcpy(server_ip, ctask->ip);
-
-	     int server_port = ctask->port;
-
-	     int cli;
-	     cli = socket(AF_INET,SOCK_STREAM,0);
-
-	     struct sockaddr_in server_addr;
-	     memset(&server_addr,0,sizeof(server_addr));
-
-	     server_addr.sin_family = AF_INET;
-	     server_addr.sin_port = htons(server_port);
-	     inet_pton(AF_INET,server_ip,&server_addr.sin_addr.s_addr);
-
-	     connect(cli,(struct sockaddr*)&server_addr,sizeof(server_addr));
-
-	     char buf [BUFSIZ];
-	     int ret,i;
-
-	     strcpy(buf, ctask->str);
-
-	     /* write, sleep, read, exit */
-	     write(cli,buf,strlen(buf));
-	     sleep(2);
-	     read(cli,buf,sizeof(buf));
-
-	     if(ctask->str[strlen(ctask->str)-1] == '\n')
-		     ctask->str[strlen(ctask->str)-1] = '\0';
+						substr = strtok_r(NULL, delim, &saveptr);
+						substr = strtok_r(NULL, delim, &saveptr);
 
 
-	     printf("Query \"%s\"\n",ctask->str);
-	     printf("%s\n",buf);
+					} while (substr);
+				}
+				else{
+					printf("unformulated input\n");
+				}
 
 
-	     /*	while(1){
+			}
+		}
+
+	}
+
+
+}
+void client_thread(CTask* ctask){
+
+	char* server_ip  = malloc(strlen(ctask->ip));
+	strcpy(server_ip, ctask->ip);
+
+	int server_port = ctask->port;
+
+	int cli;
+	cli = socket(AF_INET,SOCK_STREAM,0);
+
+	struct sockaddr_in server_addr;
+	memset(&server_addr,0,sizeof(server_addr));
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(server_port);
+	inet_pton(AF_INET,server_ip,&server_addr.sin_addr.s_addr);
+
+	connect(cli,(struct sockaddr*)&server_addr,sizeof(server_addr));
+
+	char buf [BUFSIZ];
+	int ret,i;
+
+	strcpy(buf, ctask->str);
+
+	/* write, sleep, read, exit */
+	write(cli,buf,strlen(buf));
+	sleep(2);
+	read(cli,buf,sizeof(buf));
+
+	if(ctask->str[strlen(ctask->str)-1] == '\n')
+		ctask->str[strlen(ctask->str)-1] = '\0';
+
+
+	printf("Query \"%s\"\n",ctask->str);
+	printf("%s\n",buf);
+
+
+	/*	while(1){
 		memset(buf, '\0', sizeof(buf));
 
 		fgets(buf,sizeof(buf),stdin);
@@ -117,13 +137,13 @@ int main(int argc, char *argv[]){
 		printf("ret %s\n",buf);		
 		memset(buf, '\0', strlen(buf));
 		}*/
-	     close(cli);
-	     return ;
-     }
+	close(cli);
+	return ;
+}
 
 
-     /*
-	void client_thread(CTask* ctask){
+/*
+   void client_thread(CTask* ctask){
 //lock
 char buffer[MAXLen];
 ctask->sockfd = tcp_cli(ctask->ip,ctask->port,ctask->sockfd);
